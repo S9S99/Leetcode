7a11ebba4b5e7df46f0896c18fef7642eae2178a
Siroring <whiteloli99@gmail.com>
2019-03-10 16:56:45 +0900

    Create two-sum-2-input-array-is-sorted.java

---
+++ learn/src/two-sum-2-input-array-is-sorted.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int[] result = new int[2];
+        int left = 0;
+        int right = numbers.length - 1;
+        while (left < right) {
+            if (numbers[left] + numbers[right] == target) {
+                break;
+            } else if (numbers[left] + numbers[right] < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        result[0] = left + 1;
+        result[1] = right + 1;
+        return result;
+    }
+}
